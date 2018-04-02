cc57ffc810be4633b161625f8a32db07097f548f
Ahmad Hyari <ahmad.alhyari@careem.com>
2018-04-02 14:18:37 +0400

    add base test class option

--- src/TestGenerator.js
+++ src/TestGenerator.js
@@ -1,92 +1,101 @@
-const fs = require('fs');
-const path = require('path');
-const shell = require('shelljs');
+const fs = require("fs");
+const path = require("path");
+const shell = require("shelljs");
 
 class TestGenerator {
 
-    constructor(src, unitTest, integTest, unitTestTemplate, integTestTemplate, excludedDirectories, excludedFiles, type) {
+    constructor(src, unitTest, integTest, unitTestTemplate, integTestTemplate, excludedDirectories, excludedFiles, type
+    ,baseTestClass) {
         this.srcDir = src;
         this.uniteTestDir = unitTest;
         this.integTestDir = integTest;
         this.unitTestTemplate = unitTestTemplate;
         this.integTestTemplate = integTestTemplate;
-        this.excludedFolders = excludedDirectories.split(',');
-        this.excludedFiles = excludedFiles.split(',');
+        this.excludedFolders = excludedDirectories.split(",");
+        this.excludedFiles = excludedFiles.split(",");
         this.testType = type;//create enum
+        this.baseTestClass = baseTestClass;//create enum
     };
 
     generate() {
-
-        if (this.testType == "unit") {
+        const self = this;
+        if (self.testType === "unit") {
             //generate unit test
-            this.createTestsFiles(this.srcDir, this.uniteTestDir);
-            //add base test file
-            this.copyData(this.uniteTestDir, "", "./templates/BaseTest.js");
+            self.createTestsFiles(this.srcDir, self.uniteTestDir, self.unitTestTemplate);
+            self.copyData(this.uniteTestDir, "", self.baseTestClass);
+            console.log("Unit tests generated successfully.");
         }
         else {
             //generate integration
-            this.createTestsFiles(this.srcDir, this.integTestDir);
-            this.copyData(this.integTestDir, "", "./templates/BaseTest.js");
+            self.createTestsFiles(self.srcDir, self.integTestDir, self.integTestTemplate);
+            self.copyData(self.integTestDir, "", self.baseTestClass);
+            console.log("Integration tests generated successfully.");
         }
 
 
     };
 
-    createTestsFiles(srcPath, destPath) {
-        var self = this;
+    createTestsFiles(srcPath, destPath, testTemplate) {
+        const self = this;
 
-        fs.readdirSync(srcPath).forEach(file = > {
+        fs.readdirSync(srcPath).forEach(function (file) {
 
             const fileStat = fs.statSync(srcPath + file);
-        if (fileStat.isDirectory() && !this.excludedFolders.includes(file)) {
-            fs.mkdir(destPath + file, function (err) {
-                if (err && err.code != 'EEXIST') {
-                    console.log('failed to create directory', err);
-                }
-                else {
-                    self.createTestsFiles(srcPath + file + '/', destPath + file + '/');
-                }
-            });
-        }
-        else if (path.extname(file) == '.js' && !this.excludedFiles.includes(file)) {
 
-            if (!fs.existsSync(destPath)) {
-                shell.mkdir('-p', destPath);
+            if (fileStat.isDirectory() && !self.excludedFolders.includes(file)) {
+                fs.mkdir(destPath + file, function (err) {
+                    if (err && err.code !== "EEXIST") {
+                        console.log("failed to create directory", err);
+                    }
+                    else {
+                        self.createTestsFiles(srcPath + file + "/", destPath + file + "/",testTemplate);
+                    }
+                });
             }
+            else if (path.extname(file) === ".js" && !self.excludedFiles.includes(file)) {
 
-            //TODO: make unit test file extension configurable
-            const testFileName = destPath + file.replace('.js', '.spec.js');
+                if (!fs.existsSync(destPath)) {
+                    shell.mkdir("-p", destPath);
+                }
 
-            if (!fs.existsSync(testFileName)) {
-                this.copyData(testFileName, srcPath + file);
+                //TODO: make unit test file extension configurable
+                const testFileName = destPath + file.replace(".js", ".spec.js");
+
+                if (!fs.existsSync(testFileName)) {
+                    self.copyData(testFileName, srcPath + file, testTemplate);
+                }
+
             }
+        });
 
-        }
-    })
-        ;
     };
 
-    getPathPrefix(currentPath, parentName = 'test') {
+    static getPathPrefix(currentPath, parentName = "test") {
         const depth = (currentPath.substring(currentPath.indexOf(parentName)).match(/\//g) || []).length;
         return Array(depth).join("../");
     };
 
     copyData(savPath, targetFile, fileToCopy) {
-        var data = fs.readFileSync(fileToCopy, 'utf8');
+        try {
+            let data = fs.readFileSync(fileToCopy, "utf8");
 
-        data = data
-            .replace('$$srcPath', this.getPathPrefix(savPath) + targetFile)
-            .replace('$$unitTestPath', this.getPathPrefix(savPath, 'unit'))
-            .replace('$$Class_Name', targetFile);
+            data = data
+                .replace("$$srcPath", TestGenerator.getPathPrefix(savPath) + targetFile)
+                .replace("$$unitTestPath", TestGenerator.getPathPrefix(savPath, "unit"))
+                .replace("$$Class_Name", targetFile);
 
-        fs.writeFile(savPath, data, function (err) {
-            if (err) {
-                console.log('failed to create directory', err);
-            }
-            else {
-                console.log('Test file created :' + savPath);
-            }
-        });
+            fs.writeFile(savPath, data, function (err) {
+                if (err) {
+                    console.log("failed to create directory", err);
+                }
+                else {
+                    console.log("Test file created :" + savPath);
+                }
+            });
+        } catch (err) {
+            console.log("Error reading :" + fileToCopy, "savPath : ",savPath, "targetFile : ",targetFile);
+            console.error("Error details :" + err);
+        }
     };
 
 
--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 
 'use strict';
+const path = require('path');
+const os        = require('os');
+const cli       = require('cli').enable('version');
+const fs = require('fs');
+
 const TestGenerator =  require('./TestGenerator');
 const pakg = require('../package.json');
 const version = pakg.version;
 const name = pakg.name;
-const os        = require('os');
-const cli       = require('cli').enable('version');
+const nodeModulePath= path.dirname(fs.realpathSync(__filename));
 
 cli.setApp(name, version)
 
@@ -15,12 +19,13 @@ cli.parse({
     unitTestDirectory : [ 'ut', 'target unit test directory', 'file', 'test/unit/' ],
     integrationTestDirectory: [ 'it', 'target unit test directory', 'file', 'test/integration/' ],
     unitTestTemplate : [ 'utt', 'template for unit test', 'file',
-        'test/templates/unit-test-template.js' ],
+        nodeModulePath+'/templates/unit-test-template.js' ],
     integrationTestTemplate : [ 'itt', 'template for integration test', 'file',
-        'test/templates/integration-test-template.js' ],
+        nodeModulePath+'/templates/integration-test-template.js' ],
     excludedDirectories : ['ed','comma separated directory names','string','algorithm-repository, data, routes, bin'],
     excludedFiles : ['ef','comma separated file names','string','app.js'],
     type : ['t','integration or unit','string','unit'],
+    baseTestClass : ['t','integration or unit','file',nodeModulePath+'/templates/BaseTest.js'],
 
 });
 
@@ -36,11 +41,10 @@ cli.main(function(args, options) {
             options.integrationTestTemplate,
             options.excludedDirectories,
             options.excludedFiles,
-            options.type);
+            options.type,
+            options.baseTestClass);
 
         generateUnitTests.generate();
-
-        return console.log('test generated successfully.');
     }
 
 
